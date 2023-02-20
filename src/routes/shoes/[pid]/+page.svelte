06abbdf7f81500e9f61fc17f700304102855e64e
torstenbick <torstenbrickley@oldrochester.org>
2023-02-20 15:48:58 -0500

    stufff ya

--- src/routes/shoes/[pid]/+page.svelte
+++ src/routes/shoes/[pid]/+page.svelte
@@ -10,40 +10,76 @@
     {#if data.props.shoe}
         <div class="shoe-info-card">
             <div class="shoe-display">
-            <h1>{data.props.shoe.name}</h1>
+            <h1 class="name">{data.props.shoe.name}</h1>
             <img src={data.props.shoe.img} alt={data.props.shoe.name} width="400" height="400" />
-<!--            <img scr={data.props.shoe.brandlogo} alt={data.props.shoe.brand} width="400" height="400" />-->
+<!--            <img scr={data.props.shoe.brandLogo} alt={data.props.shoe.brand} width="400" height="400" />-->
                 <div class="shoe-buy">
-                  <a href={data.props.shoe.link}>RunningWearhouse.com </a>
+                    <h2 class="review-title">Purchase</h2>
+                    <div>
+
+                    
+                <button>
+                    <a href={data.props.shoe.link}>
+                <img class="img-hov"src="https://media.licdn.com/dms/image/C4E0BAQHBmWI9w3tzog/company-logo_200_200/0/1544572615288?e=2147483647&v=beta&t=HhwAog-YBLZyc1ULuVv48MZefPUw3EHejmW25OtMwUQ" alt="running warehouse" width="100" height="100"/>
+                </a>
+                </button>
+                <button>
+                  <a href={data.props.shoe.brandLink}>
+                    <img class="img-hov"src="https://static.nike.com/a/images/f_jpg,q_auto:eco/61b4738b-e1e1-4786-8f6c-26aa0008e80b/swoosh-logo-black.png" alt="nike" width="100" height="100" />
+                </a>
+                 </button>
                 </div>
                 </div>
+                </div>
+                
             <div class="shoe-info">
                 <div class="shoe-specs">
+                    <div class="spec-section">
+                        <p><span class="text">Brand:</span></p>
+                        <p class="spec-info">{data.props.shoe.brand}</p>
+                        </div>
+                        <div class="spec-section">
+                            <p><span class="text">Price:</span></p>
+                            <p class="spec-info">${data.props.shoe.price}</p>
+                        </div>
+                        <div class="spec-section">
+                            <p><span class="text">Best Use:</span> </p>
+                            <p class="spec-info">{data.props.shoe.bestUse}</p>
+                        </div>
+                        <div class="spec-section">
+                            <p><span class="text">Surface:</span> </p>
+                            <p class="spec-info">{data.props.shoe.surface}</p>
+                        </div>
+                        <div class="spec-section">
+                            <p><span class="text">Rating:</span> </p>
+                            <p class="spec-info"><span class="stars">{data.props.shoe.rating}</span></p>
+                        </div>
+                        <div class="spec-section">
+                            <p><span class="text">Drop:</span> </p>
+                            <p class="spec-info">{data.props.shoe.drop}mm</p>
+                        </div>
+                        <div class="spec-section">
+                            <p><span class="text">Weight:</span> </p>
+                            <p class="spec-info">{data.props.shoe.weight}g</p>
+                        </div>
+                        <div class="spec-section">
+                            <p><span class="text"> Heel Stack Height:</span> </p>
+                            <p class="spec-info">{data.props.shoe.heelStackHeight}mm</p>
+                        </div>
+                        <div class="spec-section">
+                            <p><span class="text"> Forefoot Stack Height:</span> </p>
+                            <p class="spec-info">{data.props.shoe.forefootStackHeight}mm</p>
+                        </div>
+                        <div class="spec-section">
+                            <p><span class="text"> MidsolFoam:</span> </p>
+                            <p class="spec-info">{data.props.shoe.foam}</p>
+                        </div>
 
-            <p><span class="text">Brand:</span></p>
-                <p>{data.props.shoe.brand}</p>
-            <p><span class="text">Price:</span></p>
-                <p>${data.props.shoe.price}</p>
-            <p><span class="text">Best Use:</span> </p>
-                <p>{data.props.shoe.bestUse}</p>
-            <p><span class="text">Surface:</span> </p>
-                <p>{data.props.shoe.surface}</p>
-            <p><span class="text">Rating:</span> </p>
-                <p><span class="stars">{data.props.shoe.rating}</span></p>
-            <p><span class="text">Drop:</span> </p>
-            <p>{data.props.shoe.drop}</p>
-            <p><span class="text">Weight:</span> </p>
-            <p>{data.props.shoe.weight}</p>
-            <p><span class="text"> Heel Stack Height:</span> </p>
-            <p>{data.props.shoe.heelStackHeight}</p>
-            <p><span class="text"> Forefoot Stack Height:</span> </p>
-            <p>{data.props.shoe.forefootStackHeight}</p>
-            <p><span class="text"> foam:</span> </p>
-            <p>{data.props.shoe.foam}</p>
+           
             </div>
                 <div class="shoe-rewiew">
-                    <p><span class="text">Review:</span> </p>
-                    <p>{data.props.shoe.review}</p>
+                    <h2 class="review-title">Review</h2>
+                    <p class="review-text">{data.props.shoe.review}</p>
                 </div>
 
             </div>
@@ -58,6 +94,37 @@
 </section>
 
 <style>
+
+    .spec-section{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+        gap: 5px;
+    }
+    .spec-info{
+        font-weight: 700;
+        font-size: 1.1rem;
+    }
+
+    .text{
+        font-weight: 500;
+        font-size: 1rem;
+        
+    }
+    .review-title{
+        text-align: center;
+        margin: 0;
+        padding: 0;
+        font-size: 1.6rem;
+
+    }
+    .review-text{
+       font-size: 1rem;
+        font-weight: 500;
+        text-align: center;
+        line-height: 1.5;
+    }
 .shoe-info-card{
     display: grid;
     grid-template-columns: 1fr 2fr;
@@ -69,8 +136,16 @@
     border: #060c21 6px solid;
 
 }
+.shoe-info{
+    display: grid;
+    grid-template-rows: 2fr 3fr;
+    padding: 1rem;
+    
+    
+}
 .shoe-specs{
-    display: flex;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
     gap: 20px;
     justify-content: center;
     flex-wrap: wrap;
@@ -101,22 +176,21 @@
     text-align: center;
     line-height: 1.5;
 }
-h1{
+.name{
     background-color:  lightslategray;
     border-radius: 10px;
-    border: #060c21 6px solid;
+    /* border: #060c21 6px solid; */
     padding: 2px;
     width: 100%;
 }
 
 .shoe-buy{
-    display: flex;
+    display: grid;
     justify-content: center;
-    align-items: center;
     padding: 1rem;
     background-color:  lightslategray;
     border-radius: 10px;
-    border: #060c21 6px solid;
+    /* border: #060c21 6px solid; */
     margin: 1rem;
     color: #fff;
     font-size: 1.2rem;
@@ -126,9 +200,10 @@ h1{
 }
 
 .shoe-rewiew{
-    display: flex;
+    
+    display: grid;
     justify-content: center;
-    align-items: center;
+    grid-template-rows: 1fr 3fr;
     padding: 1rem;
     background-color:  lightslategray;
     border-radius: 10px;
@@ -140,4 +215,24 @@ h1{
     text-align: center;
     line-height: 1.5;
 }
+.img-hov{
+    display: block;
+    opacity: 1;
+    transition: .5s ease;
+    backface-visibility: hidden;
+
+}
+button{
+    background-color: #fff;
+    border: none;
+    padding: 0;
+    margin: 0;
+    cursor: pointer;
+
+}
+button:hover .img-hov{
+  opacity: 0.2;
+  
+}
+
 </style>
