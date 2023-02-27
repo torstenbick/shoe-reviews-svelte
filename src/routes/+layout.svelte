70ec4db3954155db1edc5ddff2f1bc9caacf5f85
torstenbick <torstenbrickley@oldrochester.org>
2023-02-27 11:21:41 -0500

    stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,7 +24,7 @@
    </main>
    <footer>
        <p>© 2021 Torsten's Running Reviews</p>
-       <a href="https://icons8.com/icon/83761/home-page">Home Page</a> icon by <a href="https://icons8.com">Icons8</a>
+       <!-- <a href="https://icons8.com/icon/83761/home-page">Home Page</a> icon by <a href="https://icons8.com">Icons8</a> -->
     </footer>
 </div>
 
