9098ad36c1d9d7e1fc0e724be52965c4adf5866e
torstenbrickley <torstenbrickley@oldrochester.org>
2023-03-07 12:37:17 -0500

    stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,16 +6,20 @@
   import Controls from "../lib/components/Controls.svelte";
 
 
-
-
 </script>
 
 
 <section class="home">
-
+<div class="maintitle">
+    <img class="title-img" src="https://images.theconversation.com/files/387060/original/file-20210301-15-1vxa1so.jpg?ixlib=rb-1.1.0&rect=0%2C208%2C1884%2C941&q=45&auto=format&w=1356&h=668&fit=crop" alt="title-image">
+</div>
 </section>
 
 <style>
+    .title-img{
+        opacity: .8;
+        width: 100
+    }
   .product-container {
     display: flex;
     gap: 20px;
